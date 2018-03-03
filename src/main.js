14de2a1089fcaa651e58d1b1023f155aba5fdefb
Sean Ruhland <sean.ruhland@gmail.com>
2018-03-02 23:48:00 -0800

    working search

--- src/main.js
+++ src/main.js
@@ -14,32 +14,173 @@ var mapOptions = {
 };
 
 
-// google maps api callback
-// this gets fired how many times?
-window.initMap = function() {
-	// create new instance of google maps api Map object
-	// using our mapOptions
-	map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-	// Set the map's style to the initial value of the selector.
 
-	// Apply new JSON when the user selects a different style.
-	document.getElementById('style-selector').addEventListener('change', function() {
-		//grab the value on the event of a selection
-		var style = document.getElementById('style-selector').value;
-		mapOptions.styles= styles[style]
-	  	map.setOptions(mapOptions);
-		console.log('map is ready')
-		// google.maps.event.trigger(map, 'resize');
-	});
+ // This example requires the Places library. Include the libraries=places
+      // parameter when you first load the API. For example:
+      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
 
-var zoomInput = document.getElementById('zoomInput').addEventListener('change', function() {
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        // var card = document.getElementById('pac-card');
+        var input = document.getElementById('pac-input');
+        // var types = document.getElementById('type-selector');
+        var strictBounds = document.getElementById('strict-bounds-selector');
+
+        // map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+
+        // Bind the map's bounds (viewport) property to the autocomplete object,
+        // so that the autocomplete requests use the current map bounds for the
+        // bounds option in the request.
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        var infowindowContent = document.getElementById('infowindow-content');
+        infowindow.setContent(infowindowContent);
+        var marker = new google.maps.Marker({
+          map: map,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+
+        // setupClickListener('changetype-all', []);
+        // setupClickListener('changetype-address', ['address']);
+        // setupClickListener('changetype-establishment', ['establishment']);
+        // setupClickListener('changetype-geocode', ['geocode']);
+        // Set the map's style to the initial value of the selector.
+
+		// Apply new JSON when the user selects a different style.
+		document.getElementById('style-selector').addEventListener('change', function() {
+			//grab the value on the event of a selection
+			var style = document.getElementById('style-selector').value;
+			mapOptions.styles= styles[style]
+		  	map.setOptions(mapOptions);
+			console.log('map is ready')
+			// google.maps.event.trigger(map, 'resize');
+		});
+
+		 var zoomInput = document.getElementById('zoomInput').addEventListener('change', function() {
 		var zoomVal = $("#zoomInput").val();
 		// mapOptions.setZoom= zoom[zoomVal]
 		var zoomInt = parseInt(zoomVal)
 	  	map.setZoom(zoomInt);
 		console.log(zoomInt)
 	});
+// 
+
+        document.getElementById('use-strict-bounds')
+            .addEventListener('click', function() {
+              console.log('Checkbox clicked! New state=' + this.checked);
+              autocomplete.setOptions({strictBounds: this.checked});
+            });
+      }
+
+
+
+
+
+
+
+
+
+// google maps api callback
+// this gets fired how many times?
+// window.initMap = function() {
+// 	// create new instance of google maps api Map object
+// 	// using our mapOptions
+// 	map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+// 	// Set the map's style to the initial value of the selector.
+
+// 	// Apply new JSON when the user selects a different style.
+// 	document.getElementById('style-selector').addEventListener('change', function() {
+// 		//grab the value on the event of a selection
+// 		var style = document.getElementById('style-selector').value;
+// 		mapOptions.styles= styles[style]
+// 	  	map.setOptions(mapOptions);
+// 		console.log('map is ready')
+// 		// google.maps.event.trigger(map, 'resize');
+// 	});
+
+// var zoomInput = document.getElementById('zoomInput').addEventListener('change', function() {
+// 		var zoomVal = $("#zoomInput").val();
+// 		// mapOptions.setZoom= zoom[zoomVal]
+// 		var zoomInt = parseInt(zoomVal)
+// 	  	map.setZoom(zoomInt);
+// 		console.log(zoomInt)
+// 	});
+// }
+
+// $( "#coordBtn" ).click(function(){
+// 	var city = $('#textBoxId').val()
+// 	$.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + city + '&key=AIzaSyDv0j8FlI2AV25URpSIXR76KBZyW6l56u4', responsHandler)
+// 	console.log(city)
+// });
+// function formSubmit(){
+// 	var city = $('#textBoxId').val()
+// 	// j
+// 	console.log(city)
+// }
+
+function responsHandler(res) {
+		console.log(res) 
+	}
+
+
+
+
+
+// function GetLatlong()
+//     {
+//         var geocoder = new google.maps.Geocoder();
+//         var address = document.getElementById('textboxid').value;
+
+//         geocoder.geocode({ 'address': address }, function (results, status) {
+
+//             if (status == google.maps.GeocoderStatus.OK) {
+//                 var latitude = results[0].geometry.location.lat();
+//                 var longitude = results[0].geometry.location.lng();
+
+//             }
+//         });
+// // 
 
 // var listener = function() {
 //   window.requestAnimationFrame(function() {
@@ -54,7 +195,6 @@ var zoomInput = document.getElementById('zoomInput').addEventListener('change', 
 //   zoom.removeEventListener("mousemove", listener);
 // });
 	
-}
 
 // 
 // var zoomDiv = document.getElementById('zoom');
